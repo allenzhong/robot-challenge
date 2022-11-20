25a612c8c10918173cc91b41511164b5b2f330a8
Allen Zhong <zhong.allen@gmail.com>
2022-11-20 18:29:49 +1300

    clean up

--- test/infrastructure/mock-command.ts
+++
@@ -1,7 +0,0 @@
-import { AbstractCommand } from "../../src/commands/abstract-command";
-
-export class MockCommand extends AbstractCommand {
-  execute(): void {
-    // not yet to implement
-  }
-}
